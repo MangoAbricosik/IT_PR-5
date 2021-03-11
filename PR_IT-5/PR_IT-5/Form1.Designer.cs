7a3a9b5e7d0fd7ef3f5dc3fa6413eb90b6a32e15
MangoAbricosik <70761769+MangoAbricosik@users.noreply.github.com>
2021-03-11 17:14:35 +0300

    Add files via upload

---
+++ PR_IT-5/PR_IT-5/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace PR_IT_5
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Обязательная переменная конструктора.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Освободить все используемые ресурсы.
+    /// </summary>
+    /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Код, автоматически созданный конструктором форм Windows
+
+    /// <summary>
+    /// Требуемый метод для поддержки конструктора — не изменяйте 
+    /// содержимое этого метода с помощью редактора кода.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.listBox1 = new System.Windows.Forms.ListBox();
+      this.button1 = new System.Windows.Forms.Button();
+      this.label1 = new System.Windows.Forms.Label();
+      this.groupBox1 = new System.Windows.Forms.GroupBox();
+      this.radioButton2 = new System.Windows.Forms.RadioButton();
+      this.radioButton3 = new System.Windows.Forms.RadioButton();
+      this.radioButton4 = new System.Windows.Forms.RadioButton();
+      this.radioButton5 = new System.Windows.Forms.RadioButton();
+      this.radioButton6 = new System.Windows.Forms.RadioButton();
+      this.groupBox1.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // listBox1
+      // 
+      this.listBox1.FormattingEnabled = true;
+      this.listBox1.Items.AddRange(new object[] {
+            "Старый любит пить пиво",
+            "Вадим мало спит",
+            "ааппаа",
+            "аауукй"});
+      this.listBox1.Location = new System.Drawing.Point(171, 52);
+      this.listBox1.Name = "listBox1";
+      this.listBox1.Size = new System.Drawing.Size(342, 212);
+      this.listBox1.TabIndex = 0;
+      // 
+      // button1
+      // 
+      this.button1.Location = new System.Drawing.Point(192, 342);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(75, 23);
+      this.button1.TabIndex = 1;
+      this.button1.Text = "button1";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(74, 301);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(35, 13);
+      this.label1.TabIndex = 2;
+      this.label1.Text = "label1";
+      // 
+      // groupBox1
+      // 
+      this.groupBox1.Controls.Add(this.radioButton6);
+      this.groupBox1.Controls.Add(this.radioButton5);
+      this.groupBox1.Controls.Add(this.radioButton4);
+      this.groupBox1.Controls.Add(this.radioButton3);
+      this.groupBox1.Controls.Add(this.radioButton2);
+      this.groupBox1.Location = new System.Drawing.Point(588, 76);
+      this.groupBox1.Name = "groupBox1";
+      this.groupBox1.Size = new System.Drawing.Size(200, 224);
+      this.groupBox1.TabIndex = 4;
+      this.groupBox1.TabStop = false;
+      this.groupBox1.Text = "groupBox1";
+      // 
+      // radioButton2
+      // 
+      this.radioButton2.AutoSize = true;
+      this.radioButton2.Location = new System.Drawing.Point(11, 39);
+      this.radioButton2.Name = "radioButton2";
+      this.radioButton2.Size = new System.Drawing.Size(85, 17);
+      this.radioButton2.TabIndex = 1;
+      this.radioButton2.TabStop = true;
+      this.radioButton2.Text = "radioButton2";
+      this.radioButton2.UseVisualStyleBackColor = true;
+      // 
+      // radioButton3
+      // 
+      this.radioButton3.AutoSize = true;
+      this.radioButton3.Location = new System.Drawing.Point(11, 70);
+      this.radioButton3.Name = "radioButton3";
+      this.radioButton3.Size = new System.Drawing.Size(85, 17);
+      this.radioButton3.TabIndex = 2;
+      this.radioButton3.TabStop = true;
+      this.radioButton3.Text = "radioButton3";
+      this.radioButton3.UseVisualStyleBackColor = true;
+      // 
+      // radioButton4
+      // 
+      this.radioButton4.AutoSize = true;
+      this.radioButton4.Location = new System.Drawing.Point(11, 93);
+      this.radioButton4.Name = "radioButton4";
+      this.radioButton4.Size = new System.Drawing.Size(85, 17);
+      this.radioButton4.TabIndex = 3;
+      this.radioButton4.TabStop = true;
+      this.radioButton4.Text = "radioButton4";
+      this.radioButton4.UseVisualStyleBackColor = true;
+      // 
+      // radioButton5
+      // 
+      this.radioButton5.AutoSize = true;
+      this.radioButton5.Location = new System.Drawing.Point(11, 136);
+      this.radioButton5.Name = "radioButton5";
+      this.radioButton5.Size = new System.Drawing.Size(85, 17);
+      this.radioButton5.TabIndex = 4;
+      this.radioButton5.TabStop = true;
+      this.radioButton5.Text = "radioButton5";
+      this.radioButton5.UseVisualStyleBackColor = true;
+      // 
+      // radioButton6
+      // 
+      this.radioButton6.AutoSize = true;
+      this.radioButton6.Location = new System.Drawing.Point(11, 171);
+      this.radioButton6.Name = "radioButton6";
+      this.radioButton6.Size = new System.Drawing.Size(85, 17);
+      this.radioButton6.TabIndex = 5;
+      this.radioButton6.TabStop = true;
+      this.radioButton6.Text = "radioButton6";
+      this.radioButton6.UseVisualStyleBackColor = true;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(800, 450);
+      this.Controls.Add(this.groupBox1);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.button1);
+      this.Controls.Add(this.listBox1);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.groupBox1.ResumeLayout(false);
+      this.groupBox1.PerformLayout();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+    }
+}
+
---
+++ PR_IT-5/PR_IT-5/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PR_IT_5
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+      // Получаем номер выделенной строки
+      int index = listBox1.SelectedIndex;
+      // Считываем строку в перменную str
+      string str = (string)listBox1.Items[index];
+      // Узнаем количество символов в строке
+      int len = str.Length;
+      string newStr = "";
+      string newStr2 = "";
+      // Считаем, что количество пробелов равно 0
+      int count = 0;
+      // Устанавливаем счетчик символов в 0
+      int i = 0, n = 0;
+      //Организуем цикл перебора всех символов в строке
+
+      if (radioButton2.Checked) n = 2;
+      if (radioButton3.Checked) n = 3;
+      if (radioButton4.Checked) n = 4;
+      if (radioButton5.Checked) n = 5;
+      if (radioButton6.Checked) n = 6;
+
+      switch (n)
+      {
+        case 2:
+          while (i < len - 1)
+          {
+            // Если нашли пробел, то увеличиваем
+            // счетчик пробелов на 1
+            if (str[i] == ' ')
+              count++;
+            i++;
+          }
+          label1.Text = "Количество пробелов = " +
+          count.ToString();
+          break;
+        case 3:
+          while (i < len - 1)
+          { 
+            if (str[i] == 'а') str = str.Replace('а', 'б');          
+            i++;
+            label1.Text = "Новая строка - " + str;
+          }
+            break;
+        case 4:
+          while (i <= len - 1)
+          {
+
+            char ch = str[i];
+            newStr += ch;
+            newStr += ch;
+            i++;
+          }
+          label1.Text = "Новая строка - " + newStr;
+          break;
+        case 5:
+          i = str.Length - 1;
+          while (i >= 0)
+          {
+            char ch = str[i];
+            newStr += ch;
+            i--;
+          }
+          label1.Text = "Новая строка - " + newStr;
+          break;
+        case 6:
+          int g = str.Length - 1;
+          i = 0;
+
+          while (g >= 0)
+          {
+            char ch = str[g];
+            newStr += ch;
+            g--;
+          }
+          if (newStr == str)
+          {
+            label1.Text = "Слово полиндром";
+          }
+          else
+          {
+            label1.Text = "Слово не полиндром";
+          }
+          break;
+      }
+    }
+  }
+}
